1d17aaa46fc17e1a2549ac43b14e6244e688c3e3
Johnno Nolan <johnnonolan@yahoo.co.uk>
2011-05-02 13:34:09 +0100

    missing files

---
+++ ChessBoard/Knight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace ChessBoard
+{
+    public class Knight : IPiece
+    {
+        public string Position { get;  set; }
+        
+
+        public Knight(string position)
+        {
+            Position = position;
+        }
+
+        public void Move(string to, string opponentsPosition)
+        {
+            if (!ValidMovePattern(to))
+                throw new InvalidOperationException("Illegal move");
+            Position = to;
+        }
+
+        bool ValidMovePattern(string to)
+        {
+            int verticalDistance = VerticalDistanceTo(to);
+            var validDistances = new[] {1, 2};
+            int horizontalDistance = HorizontalDistanceTo(to);
+            return (validDistances.Contains(verticalDistance)
+                    && validDistances.Contains(horizontalDistance) 
+                    && (verticalDistance != horizontalDistance)) ;
+            
+        }
+
+        int VerticalDistanceTo(string to)
+        {
+            
+            var fileTo = (int)(to[0]);
+            var fileFrom = (int) (Position[0]);
+            return Math.Abs(fileFrom - fileTo);
+
+        }
+
+        int HorizontalDistanceTo(string to )
+        {
+            var rankTo = to[1];
+            var rankFrom = Position[1];
+            return Math.Abs(rankFrom - rankTo);            
+        }
+
+        public void SetTaken()
+        {
+            Position = "Taken";
+        }
+    }
+}
---
+++ ChessBoard/Pawn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    public class Pawn : IPiece
+    {
+        int _validDistance;
+
+        public string Position { get; private set; }
+
+        public Pawn(string position)
+        {
+            Position = position;
+            const char homeRow = '2';
+                _validDistance = position[1] == homeRow ? 2 : 1;
+
+        }
+
+        public void Move(string to, string opponentsPosition)
+        {
+            if (!ValidStandardMovePattern(to))
+            {
+                if (TakingMovePattern(to))
+                {
+                    if (to != opponentsPosition)
+                        throw new InvalidOperationException("Illegal Move: Pawn cannot diagonally unless it is capturing a piece.");
+                    Position = to;
+                }
+                else
+                {
+                    if (HomeRowMovePattern(to))
+                        throw new InvalidOperationException("Illegal Move: pawn cannot move 2 spaces unless it in the first round and is on the home row. ");
+                    throw new InvalidOperationException("Illegal Move");
+                }
+                    
+            }
+            if ((to == opponentsPosition) && ValidStandardMovePattern(to))
+            {
+
+                throw new InvalidOperationException("pawn collides with knight. draw");
+            } 
+            Position = to;
+            _validDistance = 1;
+
+        }
+
+        bool HomeRowMovePattern(string to)
+        {
+            return (Position[0] == to[0]) && (to[1] - Position[1] > 1);
+        }
+
+        bool TakingMovePattern(string to)
+        {
+            var verticalDistance = to[1] - Position[1];
+            var horizontalDistance = (int)to[0] - Position[0];
+            return (Math.Abs(horizontalDistance) == 1) && (verticalDistance == 1);
+        }
+
+        bool ValidStandardMovePattern(string to)
+        {
+            int distance = to[1] - Position[1];
+            return Position[0] == to[0] && ( distance > 0 && distance <= _validDistance );
+        }
+
+        public void SetTaken()
+        {
+            Position = "Taken";
+        }
+    }
+}
